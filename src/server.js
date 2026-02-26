ebbe206f3b45fcb0b3947e5f3e36feb4077bb942
meepmeep <elijahsabay@gmail.com>
2026-02-26 23:21:09 +0800

    test

--- src/server.js
+++ src/server.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const app = express();
-
-// app.use(express.json());
-
-// app.get('/status', (_req, res) => {
-//   res.json({
-//     status: 'Running',
-//     timestamp: new Date().toISOString()
-//   });
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 import http from 'http';
 import app from './app.js';
 import dotenv from 'dotenv';
@@ -23,7 +7,12 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(app);
-
+app.get('/status', (_req, res) => {
+  res.json({
+    status: 'Running',
+    timestamp: new Date().toISOString()
+  });
+});
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
